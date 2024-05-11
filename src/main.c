a374d0bab7edb2e64ca1811622dbe607829e37a6
MED <marianodeleu@yahoo.com.ar>
2024-05-11 12:21:49 -0300

    encoders startup

--- src/main.c
+++ src/main.c
@@ -41,6 +41,7 @@ typedef enum {
 // Externals -------------------------------------------------------------------
 //--- Externals from timers
 volatile unsigned short timer_standby = 0;
+volatile unsigned short timer_encoders = 0;
 volatile unsigned short wait_ms_var = 0;
 unsigned char config_byte0 = 0;
 unsigned char config_byte1 = 0;
@@ -102,21 +103,11 @@ int main (void)
     IS31_Init (I2C_ADDR_P2);
     IS31_Init (I2C_ADDR_P3);
     IS31_Init (I2C_ADDR_P4);        
-    
-    // DisplayUpdateFreq (ENCODER_FRQ_P1, freq_p1);
-    // DisplayUpdateFreq (ENCODER_FRQ_P2, freq_p2);
-    // DisplayUpdateFreq (ENCODER_FRQ_P3, freq_p3);
-    // DisplayUpdateFreq (ENCODER_FRQ_P4, freq_p4);
-
-    // DisplayUpdate (ENCODER_PWR_P1, power_p1);
-    // DisplayUpdate (ENCODER_PWR_P2, power_p2);
-    // DisplayUpdate (ENCODER_PWR_P3, power_p3);
-    // DisplayUpdate (ENCODER_PWR_P4, power_p4);    
-    
+        
     // Comms_Send_Encoder_Data (ENCODER_DISPLAY_1, encoder1);    
     // Comms_Send_Encoder_Data (ENCODER_DISPLAY_2, encoder2);
 
-    equip_status_e  equip_status = EQUIP_WAIT_CONN;
+    equip_status_e equip_status = EQUIP_WAIT_CONN;
     unsigned char pix_show = 0;
 
     //-- Main Loop --------------------------    
@@ -126,21 +117,38 @@ int main (void)
         {
         case EQUIP_WAIT_CONN:
             if (!timer_standby)
-                break;
-            
-            timer_standby = 1000;
-            
-            for (int j = 0; j < 8; j++)
-                DisplaySinglePix (j, pix_show);
+            {
+                timer_standby = 1000;
+                for (int j = 0; j < 8; j++)
+                    DisplaySinglePix (j, pix_show);
 
-            if (pix_show < 24 - 1)
-                pix_show++;
-            else
-                pix_show = 0;
+                if (pix_show < 24 - 1)
+                    pix_show++;
+                else
+                    pix_show = 0;
+            }
 
             if (config_byte0 & CONF_RPI_CONNNECTED)
-                equip_status = EQUIP_FULL_CONN;
+            {
+                DisplayUpdateFreq (ENCODER_FRQ_P1, freq_p1);
+                DisplayUpdateFreq (ENCODER_FRQ_P2, freq_p2);
+                DisplayUpdateFreq (ENCODER_FRQ_P3, freq_p3);
+                DisplayUpdateFreq (ENCODER_FRQ_P4, freq_p4);
 
+                DisplayUpdate (ENCODER_PWR_P1, power_p1);
+                DisplayUpdate (ENCODER_PWR_P2, power_p2);
+                DisplayUpdate (ENCODER_PWR_P3, power_p3);
+                DisplayUpdate (ENCODER_PWR_P4, power_p4);    
+                
+                equip_status = EQUIP_FULL_CONN;
+            }
+
+            // check configs
+            if (!timer_encoders)
+            {
+                timer_encoders = 200;
+                CheckEncodersInput ();
+            }            
             break;
 
         case EQUIP_FULL_CONN:
@@ -163,9 +171,9 @@ int main (void)
             Hard_Update_Encoders ();
 
             // check encoders input from rpi
-            if (!timer_standby)
+            if (!timer_encoders)
             {
-                timer_standby = 200;
+                timer_encoders = 200;
                 CheckEncodersInput ();
             }
 
@@ -246,7 +254,7 @@ void CheckEncoderPwr (unsigned char encoder_index, unsigned char * encoder_value
 unsigned char enc_buff [8];
 void CheckEncodersInput (void)
 {
-    unsigned char buff [8];
+    unsigned char buff [10];
 
     if (I2C2_ReadMultiByte (buff, 0x44 | 0x01, 10) == 0)
     {
@@ -281,14 +289,12 @@ void TimingDelay_Decrement(void)
     if (timer_standby)
         timer_standby--;
 
-// AntennaTimeouts ();
-
-    // Treatment_Timeouts ();
+    if (timer_encoders)
+        timer_encoders--;
     
     Hard_Timeouts();
 
     I2C_Timeouts ();
-    
     
 }
 
